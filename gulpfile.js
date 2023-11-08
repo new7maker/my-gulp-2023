2c11f27517b92186c1c465c46bc7d551bf3ef857
newmaker <37245729+new7maker@users.noreply.github.com>
2023-11-08 21:31:02 +0300

    Добавил перенос изображений в папку "dist"

--- gulpfile.js
+++ gulpfile.js
@@ -18,12 +18,14 @@ import { html } from "./gulp/tasks/html.js";
 import { sass } from "./gulp/tasks/sass.js";
 import { clean } from "./gulp/tasks/clean.js";
 import { server } from "./gulp/tasks/server.js";
+import { images } from "./gulp/tasks/images.js";
 
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.scss, sass);
+    gulp.watch(path.watch.images, images);
 }
 
-gulp.task('default', gulp.series(clean, gulp.parallel(html, sass), gulp.parallel(server, watcher)));
+gulp.task('default', gulp.series(clean, gulp.parallel(html, sass, images), gulp.parallel(server, watcher)));
