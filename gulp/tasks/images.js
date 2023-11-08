b4bdece4be0b2b0e8eeafa2f6b6e6c9e6f86efa1
newmaker <37245729+new7maker@users.noreply.github.com>
2023-11-09 02:30:12 +0300

    Добавил оптимизацию изображений

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -1,5 +1,9 @@
+import imagemin from 'gulp-imagemin';
 
 export const images = () => {
     return app.gulp.src(app.path.src.images)
+        .pipe(imagemin({
+            verbose: true
+        }))
         .pipe(app.gulp.dest(app.path.build.images)); 
 }
