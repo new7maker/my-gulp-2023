b7a036524245f5c3f38c9e37fec0aeb87b123886
newmaker <37245729+new7maker@users.noreply.github.com>
2023-11-10 00:48:14 +0300

    Добавил автопрефиксер, минификация css и группирование медиа запросов

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -3,8 +3,8 @@ import imagemin from 'gulp-imagemin';
 export const images = () => {
     return app.gulp.src(app.path.src.images)
         .pipe(app.plugins.changed(app.path.build.images))
-        .pipe(imagemin({
-            verbose: true
-        }))
+        // .pipe(imagemin({
+        //     verbose: true
+        // }))
         .pipe(app.gulp.dest(app.path.build.images)); 
 }
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,13 +1,24 @@
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import groupCssMediaQueries from 'gulp-group-css-media-queries';
+import autoPrefixer from 'gulp-autoprefixer';
 import rename from 'gulp-rename';
+import cleanCss from 'gulp-clean-css'; 
 
 const sassPlugin = gulpSass(dartSass);
 
 export const sass = () => {
     return app.gulp.src(app.path.src.scss, { sourcemaps: true })
         .pipe(app.plugins.changed(app.path.build.css))
+        .pipe(app.plugins.replace(/@img\//g, '../img/'))
+        .pipe(groupCssMediaQueries())
         .pipe(sassPlugin())
+        .pipe(autoPrefixer({
+            grid: true,
+            overrideBrowserslist: ['last 3 version'],
+            cascade: true
+        }))
+        .pipe(cleanCss())
         .pipe(rename({
             basename: 'style'
         }))
