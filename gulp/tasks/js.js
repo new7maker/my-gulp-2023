cdb2b1d0893058b7cc972d42958ea4c967058f3b
newmaker <37245729+new7maker@users.noreply.github.com>
2023-11-11 00:52:31 +0300

    Добавил конвертирование и перенос шрифтов

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -2,7 +2,7 @@ import webpack from "webpack-stream";
 import { config } from "../../webpack.config.js";
 
 export const js = () => {
-    return app.gulp.src(app.path.src.js)
+    return app.gulp.src(app.path.src.js, { sourcemaps: app.isDev })
         .pipe(webpack(config))
         .pipe(app.gulp.dest(app.path.build.js))
         .pipe(app.plugins.browsersync.stream());
--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ import { html } from "./gulp/tasks/html.js";
 import { sass } from "./gulp/tasks/sass.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
+import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 import { clean } from "./gulp/tasks/clean.js";
 import { server } from "./gulp/tasks/server.js";
 import { zip } from "./gulp/tasks/zip.js";
@@ -33,8 +34,11 @@ function watcher() {
     gulp.watch(path.watch.images, images);
 }
 
+// Последовательная обработка шрифтов
+const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
+
 // Основные задачи
-const mainTasks = gulp.parallel(html, sass, images, js); 
+const mainTasks = gulp.series(fonts, gulp.parallel(html, sass, images, js)); 
 
 // Построение сценариев выполенения задач
 const dev =  gulp.series(clean, mainTasks, gulp.parallel(watcher, server));
